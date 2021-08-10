e4c62875bcfd60b783f7ce66d1cf15d8573e7797
Pat2m <88692985+Pat2m@users.noreply.github.com>
2021-08-09 20:38:23 -0400

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,93 @@
+#include <Arduino.h>
+#include <painlessMesh.h>
+#include <Arduino_JSON.h>
+
+
+#define   MESH_PREFIX     "ClimbingHoldsTest"
+#define   MESH_PASSWORD   "Rowan123"
+#define   MESH_PORT       5555
+
+//Number for this node
+int nodeNumber = 2;
+uint32_t nodeHost;
+
+//String to send to other nodes with sensor readings
+String readings;
+
+Scheduler userScheduler; // to control your personal task
+painlessMesh  mesh;
+
+// User stub
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
+String getReadings(); // Prototype for sending sensor readings
+
+//Create tasks: to send messages and get readings;
+Task taskSendMessage(TASK_SECOND * 5 , TASK_FOREVER, &sendMessage);
+
+String getReadings () {
+  JSONVar jsonReadings;
+  jsonReadings["node"] = nodeNumber;
+  jsonReadings["time"] = millis();
+  readings = JSON.stringify(jsonReadings);
+  return readings;
+}
+
+void sendMessage() {
+  String msg = getReadings();
+  mesh.sendBroadcast(msg);
+}
+
+void sendMessageHome() {
+  mesh.sendSingle(0,getReadings());
+}
+
+// Needed for painless library
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("Received from %u msg=%s\n", from, msg.c_str());
+  JSONVar myObject = JSON.parse(msg.c_str());
+  if (myObject.hasOwnProperty("Host")){
+     int nodeHosts = myObject["Host"];    
+      nodeHost = nodeHosts;
+  }
+  else{
+    int node = myObject["node"];
+    double time = myObject["time"];
+    Serial.print("Node: ");
+    Serial.println(node);
+    Serial.print("Time: ");
+    Serial.print(time);
+  }
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+  Serial.printf("New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void setup() {
+  Serial.begin(115200);
+  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+  Serial.println(mesh.getNodeId());
+  userScheduler.addTask(taskSendMessage);
+  taskSendMessage.enable();
+}
+
+void loop() {
+  // it will run the user scheduler as well
+  mesh.update();
+  sendMessageHome(); 
+}
